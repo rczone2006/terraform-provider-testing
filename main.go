ca9233329aacc7f296ae819af5c42c8478e95088
rczone2006 <rczone2006@gmail.com>
2021-05-27 22:17:23 -0700

    initial commit

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/rczone2006/terraform-provider-testing/zoning"
 
 	
 )
@@ -10,7 +10,7 @@ import (
 func main() {
     plugin.Serve(&plugin.ServeOpts{
         ProviderFunc: func() terraform.ResourceProvider {
-            return Provider()
+            return zoning.Provider()
         },
     })
 }
